164b8b97a0ae83a9f5e5f05673720365d6b64732
Franck Assuid <franckassuid@gmail.com>
2026-01-01 18:29:34 +0100

    chore: hardcode firebase config to bypass env var issues

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,12 +4,12 @@ import { getFirestore } from 'firebase/firestore';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-    appId: import.meta.env.VITE_FIREBASE_APP_ID
+    apiKey: "AIzaSyDkFYViM4aWhoHd_G9iLF7K4CdLJvJ6yUE",
+    authDomain: "feello-lappli.firebaseapp.com",
+    projectId: "feello-lappli",
+    storageBucket: "feello-lappli.firebasestorage.app",
+    messagingSenderId: "1056782510222",
+    appId: "1:1056782510222:web:fa2a89941ccfa6c1d201df"
 };
 
 const requiredKeys = ['apiKey', 'projectId', 'appId'];
