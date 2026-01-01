b890ba4259b8b1c1810d54e512fcf56c66505c58
Franck Assuid <franckassuid@gmail.com>
2026-01-01 18:25:38 +0100

    chore: explicitly load env vars in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,36 +1,45 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.png', 'apple-touch-icon.png', 'masked-icon.svg'],
-      manifest: {
-        name: 'Feello',
-        short_name: 'Feello',
-        description: 'Des questions jamais posées pour des réponses jamais entendues !',
-        theme_color: '#20473C',
-        background_color: '#20473C',
-        display: 'standalone',
-        icons: [
-          {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-            purpose: 'any maskable'
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable'
-          }
-        ]
-      }
-    })
-  ],
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    define: {
+      __APP_ENV__: JSON.stringify(env.APP_ENV),
+    },
+    plugins: [
+      react(),
+      VitePWA({
+        registerType: 'autoUpdate',
+        includeAssets: ['favicon.png', 'apple-touch-icon.png', 'masked-icon.svg'],
+        manifest: {
+          name: 'Feello',
+          short_name: 'Feello',
+          description: 'Des questions jamais posées pour des réponses jamais entendues !',
+          theme_color: '#20473C',
+          background_color: '#20473C',
+          display: 'standalone',
+          icons: [
+            {
+              src: 'pwa-192x192.png',
+              sizes: '192x192',
+              type: 'image/png',
+              purpose: 'any maskable'
+            },
+            {
+              src: 'pwa-512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+              purpose: 'any maskable'
+            }
+          ]
+        }
+      })
+    ],
+  }
 })
